ce5db5ed1f4b1f603dee4baf726ef9628574ebaf
As-swiddeeq <sideeqolabamiji0@gmail.com>
2023-05-31 08:26:16 +0100

    first commit

---
+++ js/script.js
@@ -0,0 +1,7 @@
+/* typing animation */
+var typed = new Typed(".typing", {
+    strings:["","Frontend Developer","Information/Office Manager","Personal/Virtual Assistant","Writer"],
+    typeSpeed:100,
+    BackSpeed:60,
+    loop:true
+})
